64d4e92e1075259cf8dbfc8e33c7b782ae0a7104
雅典 赤石 <aka.m6446@gmail.com>
2017-07-28 13:19:56 +0900

    first

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var cfenv = require('cfenv');
+var app = express();
+
+var fs = require('fs');
+var watson = require('watson-developer-cloud');
+var appEnv = cfenv.getAppEnv();
+var services = appEnv.getServices();
+var serviceCredentials;
+var environment_id;
+var collection_id;
+
+if (Object.keys(services).length === 0 && services.constructor === Object) {
+    if (fs.existsSync('./env.js')) {
+        Object.assign(process.env, require('./env.js'));
+    }
+    serviceCredentials = JSON.parse(process.env.VCAP_SERVICES);
+    console.log( serviceCredentials.discovery[0]); 
+    console.log( serviceCredentials); 
+    discovery = new watson.DiscoveryV1({
+        version_date: serviceCredentials.discovery[0].credentials.version,
+        username: serviceCredentials.discovery[0].credentials.username,
+        password: serviceCredentials.discovery[0].credentials.password
+    });
+    environment_id = serviceCredentials.environment_id;
+    collection_id =  serviceCredentials.collection_id;
+} else {
+    serviceCredentials = null;
+    discovery = new watson.DiscoveryV1({
+        version_date: "2016-12-01",
+        username: process.env.discovery_username,
+        password: process.env.discovery_password
+    });
+    environment_id = process.env.environment_id;
+    collection_id = process.env.collection_id;
+}
+
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res, next){
+    res.render("timeline-sample", {});
+});
+
+app.get("/query", function(req, res, next){
+    var query = Object.assign({environment_id: environment_id,
+                                collection_id: collection_id}, req.query);
+    discovery.query( query,
+    function(error, data) {
+        if ( data ) {
+            res.send(data);
+        } else {
+            console.log(error);
+            res.send(error);
+        }
+    });
+});
---
+++ env.js
@@ -0,0 +1,16 @@
+module.exports = {
+  VCAP_SERVICES: JSON.stringify({
+    "discovery": [
+      {
+        "credentials": {
+          "url": "https://gateway.watsonplatform.net/discovery/api",
+          "password": "RtAMdHWUZHQ4",
+          "username": "22199af2-57b1-4ca3-9035-6a663b3a4be2",
+          "version" : "2017-04-27"
+        }
+      }
+    ],
+    "environment_id": "da82bbda-87a6-41f2-ae97-1117cd4b6c9e",
+    "collection_id": "a21f8326-0661-4902-a7b1-239b53f84f91"
+  }),
+};
