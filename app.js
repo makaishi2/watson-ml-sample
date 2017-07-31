cce895f2e316e24d7145ab1c8b85a2ca156339ae
雅典 赤石 <aka.m6446@gmail.com>
2017-07-31 13:03:20 +0900

    app.js test

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ collection_id =  app_env.collection_id;
 
 var discovery_credentials = cfenv.getAppEnv().getServiceCreds('discovery-service-1');
 console.log(discovery_credentials);
+var discovery_env = cfenv.getAppEnv().getService('discovery-service-1');
+console.log(discovery_env);
 discovery = new watson.DiscoveryV1({
     version_date: discovery_credentials.version,
     username: discovery_credentials.username,
