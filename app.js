166712e9abf139a569d1924d42a42dad959bf418
雅典 赤石 <aka.m6446@gmail.com>
2017-08-31 13:04:06 +0900

    mod env logic

--- app.js
+++ app.js
@@ -21,16 +21,14 @@ const fs = require('fs');
 if (fs.existsSync('local.env')) {
   console.log('構成情報をlocal.envから取得します');
   require('dotenv').config({ path: 'local.env' });
-} else {
-  console.log('環境変数から構成情報を取得します');
 }
 
 // Discoveryインスタンスの生成
 const watson = require('watson-developer-cloud');
 const discovery = new watson.DiscoveryV1({
-    version_date: process.env.VERSION_DATE,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    version_date: 2017_08_01
+//    username: process.env.USERNAME,
+//    password: process.env.PASSWORD
 });
 
 const express = require("express");
